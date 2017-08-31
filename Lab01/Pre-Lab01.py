aae4f4cde9213ffba746571d6fbc4809247dd59f
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 16:08:45 -0400

    added pre-lab exercises

---
+++ Lab01/Pre-Lab01.py
@@ -0,0 +1,47 @@
+# Author:   Tony Sulfaro
+# Date :    8/31/2017
+# Purpose:  This program covers examples given in the pre-lab quiz
+
+import math
+
+AAA = 8
+BBB = 20
+C = BBB + AAA
+print( C )
+
+AAA = 8
+BBB = 20
+C = BBB * AAA
+print( C )
+
+AAA = 8
+BBB = 20
+A = BBB / AAA
+print( A )
+
+AAA = 8
+BBB = 20
+B = BBB // AAA  # Note the double slash
+print( B )
+
+AAA = 8
+BBB = 20
+C = BBB % AAA
+print( C )
+
+AAA = 2
+BBB = 5
+C = BBB ** AAA
+print( C )
+
+F = 5
+F += 1
+print( F )
+
+YYY = 17.9
+H = int(YYY)
+print( H )
+
+ZZZ = 2
+J = float(ZZZ)
+print( J )
