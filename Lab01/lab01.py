0d8f062296c83a3d48e8490bd490326f0fb14614
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 13:30:42 -0400

    commented what each part does

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -6,16 +6,20 @@ import math
 
 print("Hello this program calculates the two roots for a quadratic formula given user input.")
 
+#read in user input as string
 string_a = input("Please enter A:")
 string_b = input("Please enter B:")
 string_c = input("Please enter C:")
 
+#Convert user input from string to float
 float_a = float(string_a)
 float_b = float(string_b)
 float_c = float(string_c)
 
+#calculate positve and negative root if it exists
 positive_quad_calc = (-float_b + math.sqrt(float_b**2-4*float_a*float_c))/2*float_a
 negative_quad_calc = (-float_b - math.sqrt(float_b**2-4*float_a*float_c))/2*float_a
 
+#display output of calculation
 print("Root #1 = ", positive_quad_calc)
 print("Root #2 = ", negative_quad_calc)
