0bcaf14ad739f3123d25544de8d3d827f96e2fc2
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 13:29:11 -0400

    added in processing logic for quadratic formula and added in output

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -13,3 +13,9 @@ string_c = input("Please enter C:")
 float_a = float(string_a)
 float_b = float(string_b)
 float_c = float(string_c)
+
+positive_quad_calc = (-float_b + math.sqrt(float_b**2-4*float_a*float_c))/2*float_a
+negative_quad_calc = (-float_b - math.sqrt(float_b**2-4*float_a*float_c))/2*float_a
+
+print("Root #1 = ", positive_quad_calc)
+print("Root #2 = ", negative_quad_calc)
