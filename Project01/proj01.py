48716cfcbf5426b1aba64558ebd152e8e838d906
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 19:30:52 -0400

    refactored variable names to PascalCase

--- Project01/proj01.py
+++ Project01/proj01.py
@@ -9,20 +9,20 @@ print("You input ", rods, " rods.")
 print()
 
 #constants for calculations
-rod_meters = 5.0292
+RodMeters = 5.0292
 walking_speed = 3.1 #miles per hour
 
 #calculate units of other measurements
-meters = rod_meters*rods
-furlongs = rods/40
-miles = rods*rod_meters/1609.34
-feet = meters/.3048
-time_to_walk = miles/(walking_speed/60)
+Meters = RodMeters * rods
+Furlongs = rods / 40
+Miles = rods * RodMeters / 1609.34
+Feet = Meters / .3048
+TimeToWalk = Miles / (walking_speed / 60)
 
-#output calculations
+#output calculations (round answers to 3 decimal places)
 print("Conversions:")
-print("Meters: ", round(meters,3))
-print("Feet: ", round(feet,3))
-print("Miles: ", round(miles,3))
-print("Furlongs: ",round(furlongs,3))
-print("Minutes to walk ", rods, " rods: ", round(time_to_walk,3))
+print("Meters: ", round(Meters, 3))
+print("Feet: ", round(Feet, 3))
+print("Miles: ", round(Miles, 3))
+print("Furlongs: ", round(Furlongs, 3))
+print("Minutes to walk ", rods, " rods: ", round(TimeToWalk, 3))
