3a8373d5f5f4367aba878a90607b94b3ef509951
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 13:23:32 -0400

    added in user input

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -1,6 +1,15 @@
 # Author:   Tony Sulfaro
-# Date :    8/30/2017
-# Purpose:  This program converts
+# Date :    8/31/2017
+# Purpose:  This program solves the quadratic formula
 
 import math
 
+print("Hello this program calculates the two roots for a quadratic formula given user input.")
+
+string_a = input("Please enter A:")
+string_b = input("Please enter B:")
+string_c = input("Please enter C:")
+
+float_a = float(string_a)
+float_b = float(string_b)
+float_c = float(string_c)
