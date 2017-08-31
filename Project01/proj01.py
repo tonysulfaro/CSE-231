d8501f50ac76d485125c45fccf531a8730c7dcb5
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 17:29:58 -0400

    logic working correctly just have to round output at the end

--- Project01/proj01.py
+++ Project01/proj01.py
@@ -6,6 +6,7 @@
 user_rods = input("What will the distance in rods be? ")
 rods = float(user_rods)        #one rod is 5.0292 meters
 print("You input ", rods, " rods.")
+print()
 
 #constants for calculations
 rod_meters = 5.0292
@@ -15,11 +16,12 @@ walking_speed = 3.1 #miles per hour
 furlongs = rods/40
 miles = rods*rod_meters/1609.34
 feet = rod_meters/.3048
-time_to_walk = miles/walking_speed
+time_to_walk = miles/(walking_speed/60)
 
 #output calculations
 print("Conversions:")
 print("Meters: ", rod_meters)
 print("Feet: ", feet)
+print("Miles: ", miles)
 print("Furlongs: ",furlongs)
 print("Minutes to walk ", rods, " rods: ", time_to_walk)
