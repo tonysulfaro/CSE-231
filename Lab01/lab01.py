4bbc2462f1ca8ccf71c178fbdadd912475b2960c
Tony Sulfaro <tony116523@gmail.com>
2017-08-31 23:15:53 -0400

    Changed print statements to be more descriptive

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -4,7 +4,7 @@
 
 import math
 
-print("Hello this program calculates the two roots for a quadratic formula given user input.")
+print("Hello this program calculates the two roots for a quadratic formula given user input.\n")
 
 #read in user input as string
 InputA = input("Please enter A:")
@@ -16,10 +16,16 @@ A = float(InputA)
 B = float(InputB)
 C = float(InputC)
 
+print( "\nThe coefficients of the equation:\n" )
+print( "Coefficient A = ", A )
+print( "Coefficient B = ", B )
+print( "Coefficient C = ", C )
+
 #calculate positve and negative root if it exists
 PlusQuadCalc = (-B + math.sqrt(B ** 2 - 4 * A * C)) / 2 * A
 MinusQuadCalc = (-B - math.sqrt(B ** 2 - 4 * A * C)) / 2 * A
 
 #display output of calculation
+print( "\nThe roots of the equation:\n" )
 print("Root #1 = ", PlusQuadCalc)
 print("Root #2 = ", MinusQuadCalc)
