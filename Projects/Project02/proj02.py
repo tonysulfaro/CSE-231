981c9b805a86a5662d09dc320445e5c5c12e97d6
Tony Sulfaro <tony116523@gmail.com>
2017-09-04 22:48:11 -0400

    have seperate while loops for price and payment inside while loop with q as the terminator

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -3,18 +3,23 @@ Dimes = 10
 Quarters = 10
 Pennies = 10
 choice = ""
-price = 0.00
-payment = 0.00
+price = 0
+payment = 0
 
 while choice != "q":
-    choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
-    price = float(choice)
 
-    if price < 0:
-        print("The price you entered was negative, please enter a positive price.")
+    while price <= 0:
+        choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
+        price = float(choice)
+        price = price
 
-    payment = float(input("Please enter the payment received in xx.xx format: "))
+        if price < 0:
+            print("The price you entered was negative, please enter a positive price.")
 
-    if payment < price:
-        print("The payment amount is insufficient for the price of the item, "
-              "please pay an amount greater than or equal to the price ",price)
+    while payment > price:
+
+        payment = float(input("Please enter the payment received in xx.xx format: "))
+
+        if payment < price:
+            print("The payment amount is insufficient for the price of the item, "
+                  "please pay an amount greater than or equal to the price ", price)
