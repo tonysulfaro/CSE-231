52f2e8099042d1e224653f66dc8cd0f3e96f81f8
Tony Sulfaro <tony116523@gmail.com>
2017-09-04 23:11:25 -0400

    updated conditionals for both price and payment

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -8,19 +8,24 @@ payment = 0
 
 while choice != "q":
 
+    #check price and prompt for user input
     if price <= 0:
         choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
         price = float(choice)
+    elif price > 0:
+        1+2
     else:
         print("The price you entered was negative or not a number, please try again.")
         choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
         price = float(choice)
 
-
-while payment > price:
-
-    payment = float(input("Please enter the payment received in xx.xx format: "))
-
-    if payment < price:
+    #check payment amount and prompt for user input
+    if payment == 0:
+        payment = float(input("Please enter the payment received in xx.xx format: "))
+    elif payment < price:
         print("The payment amount is insufficient for the price of the item, "
               "please pay an amount greater than or equal to the price ", price)
+        payment = float(input("Please enter the payment received in xx.xx format: "))
+    elif payment >= price:
+        print("thanks for payment")
+        break
