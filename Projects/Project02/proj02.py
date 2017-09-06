16abbfa81bb8184fe8476e45516b8b8d22806f89
Tony Sulfaro <tony116523@gmail.com>
2017-09-05 22:26:38 -0400

    added in logic to calculate change for quarters and dimes

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -11,11 +11,11 @@ while choice != "q":
     # check price and prompt for user input
     if price == 0:
         choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
-        price = float(choice)
+        price = int(float(choice)*100)
         if price < 0:
             print("The price you entered was negative or not a number, please try again.")
             choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
-            price = float(choice)
+            price = int(float(choice) * 100)
     elif price > 0:
         1+2
     else:
@@ -25,11 +25,15 @@ while choice != "q":
 
     #check payment amount and prompt for user input
     if payment == 0:
-        payment = float(input("Please enter the payment received in xx.xx format: "))
+        payment = int(float(input("Please enter the payment received in xx.xx format: "))*100)
     elif payment < price:
         print("The payment amount is insufficient for the price of the item, "
               "please pay an amount greater than or equal to the price ", price)
-        payment = float(input("Please enter the payment received in xx.xx format: "))
+        payment = int(float(input("Please enter the payment received in xx.xx format: ")) * 100)
     elif payment >= price:
         print("thanks for payment")
         break
+change = payment - price
+changeBack = change / Quarters
+dimesBack = change - change * Dimes
+
