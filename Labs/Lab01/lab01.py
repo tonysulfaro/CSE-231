44d71a08ec601c8055cb6359f23806efc11e2932
tonysulfaro <tony116523@gmail.com>
2017-09-06 15:37:50 -0400

    removed white space from bottom

--- Labs/Lab01/lab01.py
+++ Labs/Lab01/lab01.py
@@ -31,5 +31,3 @@ root2 = (-B - math.sqrt(B ** 2 - (4 * A * C))) / (2 * A)
 print( "\nThe roots of the equation:\n" )
 print( "  Root #1 = ", round(root1,3) )  # round the result to three decimal places before printing
 print( "  Root #2 = ", round(root2,3) )
-
-
