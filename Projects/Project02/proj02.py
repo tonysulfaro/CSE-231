c40c77726a2f66f5be9af0d1391eabd191252842
Tony Sulfaro <tony116523@gmail.com>
2017-09-05 23:37:49 -0400

    changed how user input is sterilized, dont have to check to see if user enters in a string so took that logic out and it works much better.

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -9,24 +9,16 @@ while True:
     print("\nStock: {} quarters, {} dimes, {} nickles, and {} pennies".format(
         quarters, dimes, nickels, pennies))
 
-    price = input("Enter the purchase price (xx.xx) or 'q' to quit: ")
-    payment = input("Enter the payment received in (xx.xx) format or 'q' to quit: ")
+    price = int(float(input("Enter the purchase price (xx.xx) or 'q' to quit: "))*100)
+    payment = int(float(input("Enter the payment received in (xx.xx) format or 'q' to quit: "))*100)
 
-    if price.isdigit():
-        price = int(float(price) * 100)
-        if price > 0:
-            price = int(float(price) * 100)
-    else:
-        print("The value you entered is not a number or negative, please enter a valid value.")
-        price = input("Enter the purchase price (xx.xx) or 'q' to quit: ")
+    if price < 0:
+        print("The value you entered is negative, please enter a valid value.")
+        price = int(float(input("Enter the purchase price (xx.xx) or 'q' to quit: "))*100)
 
-    if payment.isdigit():
-        payment = int(float(price) * 100)
-        if payment > 0:
-            payment = int(float(price) * 100)
-    else:
-        print("The value you entered is not a number or negative, please enter a valid value.")
-        payment = input("Enter the payment amount (xx.xx) or 'q' to quit: ")
+    if payment < 0:
+        print("The value you entered is negative, please enter a valid value.")
+        payment = int(float(input("Enter the payment amount (xx.xx) or 'q' to quit: "))*100)
 
     # if user enters 'q', halt the program
     if price == 'q' or payment == 'q':
