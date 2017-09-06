9ab6b20a0ac3e69d7a85c2bf7ca71f253163f6cc
Tony Sulfaro <tony116523@gmail.com>
2017-09-05 23:08:59 -0400

    utilized project starter code to formalize formatting

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -1,39 +1,34 @@
-Nickels = 10
-Dimes = 10
-Quarters = 10
-Pennies = 10
-choice = ""
-price = 0
-payment = 0
+print("\nWelcome to change-making program.")
 
-while choice != "q":
+quarters = 10
+dimes = 10
+nickels = 10
+pennies = 10
 
-    # check price and prompt for user input
-    if price == 0:
-        choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
-        price = int(float(choice)*100)
-        if price < 0:
-            print("The price you entered was negative or not a number, please try again.")
-            choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
-            price = int(float(choice) * 100)
-    elif price > 0:
-        1+2
-    else:
-        print("The price you entered was negative or not a number, please try again.")
-        choice = input("Please enter a price in the form xx.xx or enter q to quit: ")
-        price = float(choice)
+while True:
 
-    #check payment amount and prompt for user input
-    if payment == 0:
-        payment = int(float(input("Please enter the payment received in xx.xx format: "))*100)
-    elif payment < price:
-        print("The payment amount is insufficient for the price of the item, "
-              "please pay an amount greater than or equal to the price ", price)
-        payment = int(float(input("Please enter the payment received in xx.xx format: ")) * 100)
-    elif payment >= price:
-        print("thanks for payment")
+    print("\nStock: {} quarters, {} dimes, {} nickles, and {} pennies".format(
+        quarters, dimes, nickels, pennies))
+
+    in_str = input("Enter the purchase price (xx.xx) or 'q' to quit: ")
+
+    # if user enters 'q', halt the program
+    if in_str == 'q':
         break
+
+    # Fill in the good stuff here instead of the following print
+    print("Testing:", in_str)
+
 change = payment - price
-changeBack = change / Quarters
-dimesBack = change - change * Dimes
+print(change)
+quartersBack = change // quarters
+temp = change - quartersBack * quarters
+dimesBack = temp // dimes
+temp = temp - dimesBack * dimes
+nickelsBack = temp // nickels
+temp = temp - nickelsBack * nickels
+penniesBack = temp // pennies
+temp = temp - penniesBack * pennies
+
+print("The change back is:", quartersBack, "quarters",dimesBack, "dimes", nickelsBack, "nickels and",penniesBack, "pennies.")
 
