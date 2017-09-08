57a190ab39044486ee735a389222479bdbcbec6b
Tony Sulfaro <tony116523@gmail.com>
2017-09-07 20:31:24 -0400

    trying to sort how how to compensate for user wanting to quit the program.

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -3,14 +3,21 @@ dimes = 10
 nickels = 10
 pennies = 10
 
-while True:
+price = input(u"Enter the purchase price (xx.xx) or 'q' to quit: ")
+if price == "q":
+    exit
+payment = input(u"Enter the payment received in (xx.xx) format or 'q' to quit: ")
+if payment == "q":
+    exit
+
+while price != "q" or payment != "q":
     print()
     print("\nWelcome to change-making program.")
     print("\nStock: {} quarters, {} dimes, {} nickles, and {} pennies".format(
         quarters, dimes, nickels, pennies))
 
-    price = int(float(input("Enter the purchase price (xx.xx) or 'q' to quit: "))*100)
-    payment = int(float(input("Enter the payment received in (xx.xx) format or 'q' to quit: "))*100)
+    price = float(price)
+    payment = float(payment)
 
     if price < 0:
         print("The value you entered is negative, please enter a valid value.")
@@ -20,9 +27,7 @@ while True:
         print("The value you entered is negative, please enter a valid value.")
         payment = int(float(input("Enter the payment amount (xx.xx) or 'q' to quit: "))*100)
 
-    # if user enters 'q', halt the program
-    if price == 'q' or payment == 'q':
-        break
+
 
     change = payment - price
 
