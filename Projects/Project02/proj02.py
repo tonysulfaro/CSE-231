dc9e80f6ecefd7a790f5a18935324bae32a30405
Tony Sulfaro <tony116523@gmail.com>
2017-09-14 18:12:06 -0400

    trying again. change making logic is broken atm

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -11,25 +11,16 @@ print("\nWelcome to change-making program.")
 print("\nStock: {} quarters, {} dimes, {} nickels, and {} pennies".format(
         quarters, dimes, nickels, pennies))
 
-price = input(u"Enter the purchase price (xx.xx) or 'q' to quit: ")
-if type(price) is int:
-    price = int(price)*100
-else:
-    quit()
+price = int(float(input("Enter the purchase price (xx.xx) or 'q' to quit: "))*100)
 
-payment = input(u"Input dollars paid (int): ")
-if type(payment) is int:
-    payment = int(payment)*100
-else:
-    quit()
+
+payment = int(float(input("Input dollars paid (int): "))*100)
+
 
 while price != 'q' or payment != 'q':
 
     if price != 'q' and payment != 'q':
 
-        # convert price and payment to raw cent amount
-        price = int(float(price) * 100)
-        payment = int(float(payment) * 100)
 
         # Check for negative values
         if price < 0:
@@ -67,11 +58,11 @@ while price != 'q' or payment != 'q':
         if quartersBack > 0:
             ChangeString += "Quarters: " + str(quartersBack)
         if dimesBack > 0:
-            ChangeString += "Dimes: " + str(dimesBack)
+            ChangeString += " Dimes: " + str(dimesBack)
         if nickelsBack > 0:
-            ChangeString += "Nickels: " + str(nickelsBack)
+            ChangeString += " Nickels: " + str(nickelsBack)
         if penniesBack > 0:
-            ChangeString += "Pennies: " + str(penniesBack)
+            ChangeString += " Pennies: " + str(penniesBack)
 
         # print stock
         if payment == price:
@@ -83,14 +74,8 @@ while price != 'q' or payment != 'q':
 
         print("\nStock: {} quarters, {} dimes, {} nickels, and {} pennies".format(
             quarters, dimes, nickels, pennies))
+
     #input price and payment amount
-    price = input(u"Enter the purchase price (xx.xx) or 'q' to quit: ")
-    if type(price) is int:
-        price = int(price)*100
-    else:
-        break
-    payment = input(u"Input dollars paid (int): ")
-    if type(payment) is int:
-        payment = int(payment)*100
-    else:
-        break
+    price = int(float(input("Enter the purchase price (xx.xx) or 'q' to quit: ")) * 100)
+
+    payment = int(float(input("Input dollars paid (int): ")) * 100)
