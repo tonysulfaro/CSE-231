8b8556ed3efe6e907c8622a919173b757b0f58c0
tonysulfaro <tony116523@gmail.com>
2017-09-14 20:25:30 -0400

    removed print statements for debugging

--- Projects/Project02/proj02.py
+++ Projects/Project02/proj02.py
@@ -84,7 +84,7 @@ while price != 'q' or payment != 'q':
             pennies -= 1
 
         if change > 0:
-            print("\nThere is not enough stock to make the change provided. Sorry")
+            print("\nError: ran out of coins.")
             break
 
 
@@ -110,16 +110,17 @@ while price != 'q' or payment != 'q':
             quarters, dimes, nickels, pennies))
 
     #input price and payment amount
+
     price = input("Enter the purchase price (xx.xx) or 'q' to quit: ")
 
-    if type(price) is not str:
-        price = int(float(price)* 100)
-    else:
+    if price == 'q':
         quit()
+    else:
+        price = int(float(price) * 100)
 
     payment = input("Input dollars paid (int): ")
 
-    if type(payment) is not str:
-        payment = int(float(payment)* 100)
-    else:
+    if payment == "q":
         quit()
+    else:
+        payment = int(float(payment) * 100)
