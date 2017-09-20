8c4f642d0866000846027bcec31390fbda3704f4
tonysulfaro <tony116523@gmail.com>
2017-09-20 15:05:04 -0400

    changed project 3 location

--- Projects/proj03.py
+++
@@ -1,9 +0,0 @@
-# assume a word has at least one vowel
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
-
-# your code goes here
-
-# here are two lines of the output -- you need to add a third line
-# print("\n"+"="*12)
-# print("{:8s}{:7s} | {:12s}{:7s}".format("vowels","length","consonants","length"))
