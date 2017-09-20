a960b6c9a3adc7ab829d298e30e41f788f2ac61c
Tony Sulfaro <tony116523@gmail.com>
2017-09-20 16:25:10 -0400

    cleaned up formatting and whitespace. added condition if someone enters in a one letter consonant just add "ay" to it.

--- Labs/Lab03/lab03.py
+++ Labs/Lab03/lab03.py
@@ -1,16 +1,18 @@
 vowels = "aeiou"
 
+#user input
 word = input("Enter a word ('quit' to quit): ")
 word = word.lower()
 
 if word == 'quit':
     quit()
 
+#mainloop
 while word != 'quit':
 
     if word[0] in vowels:
         word = word + "way"
-    elif len(word) == 0:
+    elif word[0] not in vowels and len(word) == 1:
         word = word + 'ay'
     else:
         for i, ch in enumerate(word):
@@ -23,5 +25,3 @@ while word != 'quit':
     #user input
     word = input("Enter a word ('quit' to quit): ")
     word = word.lower()
-
-
