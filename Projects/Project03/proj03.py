195d78a1761d6ea6ce8e9a051f91a52bfaa8a536
Tony Sulfaro <tony116523@gmail.com>
2017-09-20 16:55:42 -0400

    getting closer. Looks like the vowel string works but the consonant one needs some work. Excellent progress for such little time spent.

--- Projects/Project03/proj03.py
+++ Projects/Project03/proj03.py
@@ -1,15 +1,34 @@
 # assume a word has at least one vowel
 vowels = "aeiou"
-count = 0
+vowelCount = 0
+consonantCount = 0
+vowelString = ""
+consonantString = ""
 
 word = input("Enter a word.")
 word = word.lower()
 
 # your code goes here
-while word != 'quit' and count < 5:
-    print(word)
+while len(vowelString) < 5 or len(consonantString) < 5:
 
+    for i, ch in enumerate(word):
+        if ch in vowels:
+            if ch not in vowelString:
+                vowelCount += 1
+                vowelString += ch
+        else:
+            if ch not in consonantString:
+                consonantCount += 1
+                consonantString += ch
+
+    print("\n" + "=" * 12)
+    print("{:8s}{:7s} | {:12s}{:7s}".format("vowels", "length", "consonants", "length"))
+    print(vowelString, vowelCount, consonantString, consonantCount)
+
+    word = input("Enter a word.")
+    word = word.lower()
 
 # here are two lines of the output -- you need to add a third line
-# print("\n"+"="*12)
-# print("{:8s}{:7s} | {:12s}{:7s}".format("vowels","length","consonants","length"))
+print("\n"+"="*12)
+print("{:8s}{:7s} | {:12s}{:7s}".format("vowels","length","consonants","length"))
+print(vowelString,vowelCount,consonantString,consonantCount)
