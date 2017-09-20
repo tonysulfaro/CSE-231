144c61e60387c4a2626fbaccccfaa571b6447d6f
tonysulfaro <tony116523@gmail.com>
2017-09-20 14:59:54 -0400

    added in logic to handle user input inside and outside of the loop

--- Labs/Lab03/lab03.py
+++ Labs/Lab03/lab03.py
@@ -1,5 +1,18 @@
-VOWELS = "aeiou"
+vowels = "aeiou"
+
 word = input("Enter a word ('quit' to quit): ")
+word = word.lower()
+print(word)
+
+if word == 'quit':
+    quit()
+
+while word != 'quit':
+
+    word = input("Enter a word ('quit' to quit): ")
+    word = word.lower()
+    print(word)
+
 
 # Error message used in Mimir test
 # print("Can't convert empty string.  Try again.")
