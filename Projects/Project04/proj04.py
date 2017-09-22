95200457d00de9ed58258bfc1c83c7644dba44e1
Tony Sulfaro <tony116523@gmail.com>
2017-09-22 13:27:14 -0400

    initial commit of project 4 skeleton code

---
+++ Projects/Project04/proj04.py
@@ -0,0 +1,30 @@
+def get_ch():
+    # prompt for the input in a loop
+    ch = input("Enter a character or press the Return key to finish: ")
+
+    # in case of invalid input, print the following error message
+    print("Invalid input, please try again.")
+
+    # return the ch at the end
+
+
+def find_state(state, ch):
+    pass
+
+
+def main():
+    print("I can recognize if you are laughing or not.")
+    print("Please enter one character at a time.")
+
+    # initialize the variables, for example:
+    string = ""
+
+    # call the functions in a loop
+
+    # when user enters an empty string, you should print the results
+    print("\nYou entered", string)
+    print("You are laughing.")
+    print("You are not laughing.")
+
+
+main()
