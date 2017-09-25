3b6b4f10935609e90c9cab47b03c2ea310de2443
Tony Sulfaro <tony116523@gmail.com>
2017-09-24 23:43:26 -0400

    Added in logic to handle user input in the get_ch function. handles if its longer than 1 it reprompts until it is. if its a char or null then it returns it.

--- Projects/Project04/proj04.py
+++ Projects/Project04/proj04.py
@@ -14,9 +14,13 @@ def get_ch():
     ch = input("Enter a character or press the Return key to finish: ")
 
     # in case of invalid input, print the following error message
-    print("Invalid input, please try again.")
+    while len(ch) > 1:
+        print("Invalid input, please try again.")
+        ch = input("Enter a character or press the Return key to finish: ")
 
     # return the ch at the end
+    if len(ch) == 1 or ch == "":
+        return ch
 
 
 def find_state(state, ch):
