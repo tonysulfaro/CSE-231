c9aca99f2fed5500196dcce4f7519f489cfb0ddd
Tony Sulfaro <tony116523@gmail.com>
2017-09-27 17:59:02 -0400

    Commented out print statements to pass mimir tests

--- Labs/Lab04/lab04.py
+++ Labs/Lab04/lab04.py
@@ -1,24 +1,24 @@
 #Lab 04
 
 #Part A: Leap Year
-year = int(input("input a year: "))
+#year = int(input("input a year: "))
 
 def leap_year(year):
 
     return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 
-print(leap_year(year))
+#print(leap_year(year))
 
 #Part B: Rotate
-s = input("Input a String: ")
-n = int(input("Input an Int: "))
+#s = input("Input a String: ")
+#n = int(input("Input an Int: "))
 def rotate(s,n):
     return s[-n:]+s[:-n]
-print(rotate(s,n))
+#print(rotate(s,n))
 
 #Part C: Digit Count
-numInput = int(float(input("Enter in an Int or Float: ")))
-numInput = str(numInput)
+#numInput = int(float(input("Enter in an Int or Float: ")))
+#numInput = str(numInput)
 evens = '2468'
 odds = '13579'
 
@@ -38,10 +38,10 @@ def digit_count(numInput):
                 zeroCount += 1
 
     return (evenCount,oddCount,zeroCount)
-print(digit_count(numInput))
+#print(digit_count(numInput))
 
 #Part D: Float Check
-userString = input("Enter a String to see if it's a Digit: ")
+#userString = input("Enter a String to see if it's a Digit: ")
 
 def float_check(userString):
     decimalCount = 0
@@ -57,4 +57,4 @@ def float_check(userString):
 
     return decimalCount <= 1
 
-print(float_check(userString))
+#print(float_check(userString))
