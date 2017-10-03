e465f177acf2e8a0aa1c9177d9896e3b75748c0f
Tony Sulfaro <tony116523@gmail.com>
2017-10-03 01:01:11 -0400

    Overhauled the get_ch method to be a loop instead of prompting for user input recursively due to some technical thing that is beyond my understanding. Oh well. at least i tried to use recursion but guess it wasn't meant to be.

--- Projects/Project04/proj04.py
+++ Projects/Project04/proj04.py
@@ -11,17 +11,17 @@
 
 def get_ch():
     # prompt for the input in a loop
-    ch = input("Enter a character or press the Return key to finish: ")
-    ch = str(ch)
+    while True:
+        ch = input("Enter a character or press the Return key to finish: ")
+        ch = str(ch)
 
-    # in case of invalid input, print the following error message
-    if len(ch) > 1:
-        print("Invalid input, please try again.")
-        get_ch()
-    elif len(ch) == 1 or ch == "":
-        return ch
-    else:
-        get_ch()
+        # in case of invalid input, print the following error message
+        if len(ch) > 1:
+            print("Invalid input, please try again.")
+        elif len(ch) == 1 or ch == "":
+            return ch
+        else:
+            print("Invalid input, please try again.")
 
 
 def find_state(state, ch):
