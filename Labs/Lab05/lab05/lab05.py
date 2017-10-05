fa543611541be0eb23fd4fe9f9383b368c1f6271
Tony Sulfaro <tony116523@gmail.com>
2017-10-04 22:19:36 -0400

    misc formatting

--- Labs/Lab05/lab05/lab05.py
+++ Labs/Lab05/lab05/lab05.py
@@ -1,21 +1,28 @@
+import sys
+
+#open files to read and write
 fp = open("data.txt")
 fp.readline()
 outfile = open("output.txt",'w')
 
+#cumulative totals
 heightTotal = 0
 weightTotal = 0
 BMITotal = 0
 peopleTotal = 0
 
+#singular values
 maxHeight = 0
-minHeight = 999999
+minHeight = sys.maxsize
 maxWeight = 0
-minWeight = 999999
+minWeight = sys.maxsize
 maxBMI = 0
-minBMI = 999999
+minBMI = sys.maxsize
 
+#header
 print("{:<12s}{:<12s}{:<12s}{:<12s}".format("Name", "Height(m)", "Weight(kg)", "BMI"),file = outfile)
 
+#for each line in openfile
 for line in fp:
     #print(line)
     height = float(line[12:24])
@@ -43,10 +50,12 @@ for line in fp:
 
     print("{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(line[0:12],height,weight,BMI),file = outfile)
 
+#calculate averages
 heightAverage = heightTotal / peopleTotal
 weightAverage = weightTotal / peopleTotal
 BMIAverage = BMITotal / peopleTotal
 
+#print min,max, average
 print("\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Average",heightAverage,weightAverage,BMIAverage),file = outfile)
 print("{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Max",maxHeight,maxWeight,maxBMI),file = outfile)
 print("{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Min",minHeight,minWeight,minBMI),file = outfile)
