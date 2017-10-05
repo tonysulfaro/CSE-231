297e6bdad4abbf706382562fec1e3e6fb7cd4317
Tony Sulfaro <tony116523@gmail.com>
2017-10-05 01:22:39 -0400

    trying to finaggle it in there but it is having none of it. doing state changes to keep track of when to print out results

--- Projects/Project05/proj05/proj05.py
+++ Projects/Project05/proj05/proj05.py
@@ -45,12 +45,14 @@ def main():
     print()
     ## read the file
 
-    state = 1
+    state = 0
+    stateBefore = 0
+    currentProduct = ""
+
     bestPerforming = ""
     sales = 0
     bestROI = 0.0
     bestROIad = ""
-    currentProduct = ""
 
     for line in fp:
 
@@ -62,32 +64,53 @@ def main():
         productPrice = float(line[-16:-8].strip())
         productionCost = float(line[-8:].strip())
 
-        #pass variables onto functions
+        # pass variables onto functions
         totalRevenue = revenue(salesNumber, productPrice)
         costofGoods = cost_of_goods_sold(placementCount, placementCost, salesNumber, productionCost)
         roi = calculate_ROI(revenue(salesNumber, productPrice),
                             cost_of_goods_sold(placementCount, placementCost, salesNumber, productionCost))
         totalProdCost = salesNumber * productionCost
 
-        find_state(state,product)
 
-        if currentProduct != product:
-
+        if state == 0:
+            print("case 1")
             if salesNumber > sales:
                 sales = salesNumber
                 bestPerforming = ad
             if roi > bestROI:
                 bestROI = roi
                 bestROIad = ad
-
+            currentProduct = product
+            state += 1
+        elif state == stateBefore and product == currentProduct:
+            print("case 2")
+            if salesNumber > sales:
+                sales = salesNumber
+                bestPerforming = ad
+            if roi > bestROI:
+                bestROI = roi
+                bestROIad = ad
+            stateBefore+=1
         else:
-            print(state)
+            print("case 3")
             print("\n" + product)
             print("  {:27s}{:>11s}".format("Best-Performing Ad", "sales"))
             print("  {:27s}{:>11d}".format(bestPerforming, sales))
             print("\n  {:27s}{:>11s}".format("Best ROI", "percent"))
             print("  {:27s}{:>10.2f}%".format(ad, bestROI))
 
+            bestPerforming = ""
+            sales = 0
+            bestROI = 0.0
+            bestROIad = ""
+
+            state = 0
+            stateBefore = 0
+            currentProduct = ""
+
+            state+=1
+            stateBefore+=1
+            currentProduct = product
 
 
 if __name__ == "__main__":
