5295473f58e98e9c03b04fac84ba4e65f17a7102
Tony Sulfaro <tony116523@gmail.com>
2017-10-11 00:00:03 -0400

    just playing around

--- Labs/Lab06/lab06.py
+++ Labs/Lab06/lab06.py
@@ -4,4 +4,6 @@ student_list = [[]]
 
 print("{:20s}{:6s}{:6s}{:6s}{:6s}{:6s}".format("Name", "Exam1", "Exam2", "Exam3", "Exam4", "Mean"))
 for line in fp:
-    pass
+    line = line.replace(" ", "")
+    student_list.__add__(list(line))
+    print(student_list)
