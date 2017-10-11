91c21548fb8f84128aa7d9e2ca827181c3deb27f
tonysulfaro <tony116523@gmail.com>
2017-10-11 15:46:53 -0400

    finished it

--- Labs/Lab06/lab06.py
+++ Labs/Lab06/lab06.py
@@ -1,10 +1,18 @@
 fp = open("scores.txt")
+counter = 0
 
 student_list = []
+total_list = [0,0,0,0]
 
-print("{:20s}{:6s}{:6s}{:6s}{:6s}{:6s}".format("Name", "Exam1", "Exam2", "Exam3", "Exam4", "Mean"))
+
+
+
+
+print("{:20s}{:>6s}{:>6s}{:>6s}{:>6s}{:>10s}".format("Name", "Exam1", "Exam2", "Exam3", "Exam4", "Mean"))
 
 for line in fp:
+    counter += 1
+
     line = line.strip("\n").strip()
     line = line.split()
 
@@ -13,9 +21,13 @@ for line in fp:
     exam2 = int(line[3])
     exam3 = int(line[4])
     exam4 = int(line[5])
-    average = round(((exam1+exam2+exam3+exam4)/4.0), 2)
+    average = (exam1+exam2+exam3+exam4)/4
+    average = round(average,2)
 
-    print(line)
+    total_list[0] += exam1
+    total_list[1] += exam2
+    total_list[2] += exam3
+    total_list[3] += exam4
 
     tuple = (name,exam1,exam2,exam3,exam4,average)
     student_list.append(tuple)
@@ -23,4 +35,9 @@ for line in fp:
 student_list_sorted = sorted(student_list)
 
 for item in student_list_sorted:
-    print("{:20s}{:6d}{:6d}{:6d}{:6d}{:6f}".format(item[0], item[1], item[2], item[3], item[4], item[5]))
+    print("{:20s}{:6d}{:6d}{:6d}{:6d}{:10.2f}".format(item[0], item[1], item[2], item[3], item[4], item[5]))
+
+mean_list = [total_list[0]/counter,total_list[1]/counter,total_list[2]/counter,total_list[3]/counter]
+
+print("{:20s}{:6.1f}{:6.1f}{:6.1f}{:6.1f}".format("Exam Mean",mean_list[0], mean_list[1], mean_list[2], mean_list[3]))
+
