4766a57e7e197fcb4c07cf9be4a37fa5e9bafcb4
tonysulfaro <tony116523@gmail.com>
2017-10-18 16:02:58 -0400

    finished lab07

--- Labs/Lab07/lab07.py
+++ Labs/Lab07/lab07.py
@@ -1,13 +1,49 @@
 def open_file():
-    fp = ""
-
+    '''Remember to put a docstring here'''
     while True:
-
-        file_name = input("Enter a filename: ")
+        file_name = input("Input a file name: ")
         try:
+
             fp = open(file_name)
+            break
         except FileNotFoundError:
-            print("Error invalid filename.")
+            print("Unable to open file. Please try again.")
+            continue
+    return fp
+
 
 def main():
-    pass
+
+    count = [0 for i in range(9)]
+    percentages = [0 for i in range(9)]
+    expected_percentages = ['30.1','17.6','12.5','9.7','7.9','6.7','5.8','5.1','4.6']
+
+    fp = open_file()
+
+    for line in fp:
+
+        line = line.strip()
+
+        try:
+            number = int(line)
+        except (TypeError, ValueError):
+            pass
+
+        try:
+            number = int(str(line[0]))
+        except ValueError:
+            pass
+
+        index = number - 1
+        count[index]+= 1
+
+
+    line_count = sum(count)
+
+    print("Digit Percent Benford")
+    for x in range(len(count)):
+        percentages[x] = (count[x]/line_count)*100
+        print("{:3s} {:>10.2f}% ({:>5s}%) ".format(str(x+1)+":",percentages[x],expected_percentages[x]))
+
+
+main()
