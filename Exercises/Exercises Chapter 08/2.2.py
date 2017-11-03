93e67064c9d4e99ec48c5bddc9c57519d13efa33
Tony Sulfaro <tony116523@gmail.com>
2017-11-02 22:31:25 -0400

    finished ch8 ex2

--- Exercises/Exercises Chapter 08/2.2.py
+++ Exercises/Exercises Chapter 08/2.2.py
@@ -1,25 +1,28 @@
 # sort_list() function goes here
-def sort_list(a_list):
-    return sorted(a_list)
+a_list = list()
 
+def sort_list(the_list):
+    global a_list
+    a_list = sorted(the_list)
 
 def main():
-
-    a_list = []
-
     # loop to accept integers until an empty string is entered goes here
-    user_input = input(" ")
-    while user_input != "":
+    user_input = input()
+
+
+    while user_input != '':
         try:
             user_input = int(user_input)
             a_list.append(user_input)
-        except (ValueError, TypeError):
-            print("That's not an Integer.")
-        user_input = input(" ")
+        except(TypeError, ValueError):
+            print("You can't enter non-numbers")
+            quit()
+        user_input = input()
+
 
     ######Do not modify this part######
     print(a_list)
-    a_list = sort_list(a_list)
+    sort_list(a_list)
     print(a_list)
     ######Do not modify this part######
     ######main() ends here
