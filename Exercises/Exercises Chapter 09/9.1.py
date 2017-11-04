9f34f7f92ecdb996c59e07e9a86168d0470387bf
tonysulfaro <tony116523@gmail.com>
2017-11-04 16:21:02 -0400

    finished 9.1 hw

--- Exercises/Exercises Chapter 09/9.1.py
+++ Exercises/Exercises Chapter 09/9.1.py
@@ -1,10 +1,18 @@
 dictlist = []
+the_dict = dict()
 #declare variables
+
+choice = ""
 #loop needed
-input("Name: ")
-input("Number: ")
-##add name and number to dictionary
-input('More data (y/n)? ')
+while choice != 'n':
+    name = input("Name: ")
+    number = input("Number: ")
+
+    #add name and number to dictionary
+    the_dict[name] = number
+
+    choice = input('More data (y/n)? ').lower()
+
 #if more data, then repeat
 
 for key, value in the_dict.items():  #we store the dictionary in a list, then sort and print
