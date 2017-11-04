de84c28bcdfa09e2c274b827c0b7f58e6ad59049
tonysulfaro <tony116523@gmail.com>
2017-11-04 16:25:27 -0400

    finished 9.2 hw

--- Exercises/Exercises Chapter 09/9.2.py
+++ Exercises/Exercises Chapter 09/9.2.py
@@ -1,3 +1,5 @@
+import string
+
 def open_file():
     fpointer = open('example.txt')
     return fpointer
@@ -6,7 +8,18 @@ def open_file():
 def main():
     dictlist = []
     fp = open_file()
+    dict_of_words = dict()
     # loop to iterate over lines in file
+    for line in fp:
+        word_list = line.split()
+
+        for word in word_list:
+            word = word.strip().strip(string.punctuation).lower()
+
+            if word not in dict_of_words:
+                dict_of_words[word] = 0
+
+            dict_of_words[word] += 1
 
     for key, value in dict_of_words.items():
         temp = (key, value)
