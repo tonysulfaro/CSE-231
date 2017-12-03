b42562221c9813d3dde3bbfc1ffc57844daa6366
Tony Sulfaro <tony116523@gmail.com>
2017-12-03 15:46:19 -0500

    intitial commit of lab13 base files

---
+++ Labs/Lab13/lab13_test1.py
@@ -0,0 +1,32 @@
+
+##
+## Demonstrate some of the operations of the Pet classes
+##
+
+import pets
+
+
+def main():
+    
+    try:
+
+        A = pets.Pet( "hamster" )
+        print( A )
+              
+        # Dog named Fido who chases Cats
+        B = pets.Dog( "Fido" )
+        print( B )
+
+        # Cat named Fluffy who hates everything
+        C = pets.Cat( "Fluffy", "everything" )
+        print( C )
+
+        D = pets.Pet( "pig" )
+        print( D )
+        
+    except pets.PetError:
+        
+        print( "Got a pet error." )
+
+main()
+
